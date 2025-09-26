12e115b087777ef0d65f7720077d7566ba0f52e2
wangzhenghao <2806008856@qq.com>
2025-09-26 18:26:06 +0800

    base doc

--- src/main/java/com/openapi/springdoc/aspect/ApiLogAspect.java
+++ src/main/java/com/openapi/springdoc/aspect/ApiLogAspect.java
@@ -1,5 +1,6 @@
 package com.openapi.springdoc.aspect;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openapi.springdoc.annotation.ApiLog;
 import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.LogManager;
@@ -8,15 +9,22 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.util.Arrays;
 import java.util.Objects;
 
-@Log4j2
 @Aspect
 @Component
 public class ApiLogAspect {
 //    private static final Logger log = LogManager.getLogger(ApiLogAspect.class);
+
+    private final ObjectMapper objectMapper;
+
+    public ApiLogAspect(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     //切入点：所有标记了@ApiLog的方法
     @Pointcut("@annotation(com.openapi.springdoc.annotation.ApiLog)")
     public void apiLogPointCut(){}
@@ -25,6 +33,8 @@ public class ApiLogAspect {
     public Object aroundApiLog(ProceedingJoinPoint joinPoint) throws Throwable {
         //获取方法签名
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        //获取使用注解的方法名
+        String methodName = methodSignature.getMethod().getName();
 
         //获取方法上的@ApiLog注解
         ApiLog apiLog = methodSignature.getMethod().getAnnotation(ApiLog.class);
@@ -37,28 +47,27 @@ public class ApiLogAspect {
         //获取当前切片名
         String aspectName = this.getClass().getSimpleName();
 
-        //获取目标类和动态logger
         //获取目标方法所在类（接口或实现类）
         Class<?> targetClass = methodSignature.getDeclaringType();
         String classFullName = targetClass.getName();
-        String methodName = methodSignature.getMethod().getName();
 
         //获取方法参数
-        Object[] args = joinPoint.getArgs();
+        String args = objectMapper.writeValueAsString(joinPoint.getArgs());
         //动态创建Logger
         Logger targetLogger = LogManager.getLogger(targetClass);
-
-
-        targetLogger.info(":Aspect:{}, 调用接口:{} {}, 参数:{}, 描述:{}", aspectName, classFullName, methodName, Arrays.toString(args), desc);
+        targetLogger.info(":Aspect:{}, 调用接口:{} {}, 参数:{}, 描述:{}",
+                aspectName, classFullName, methodName, args, desc);
 
         Object result;
         long startTime = System.currentTimeMillis();
         try {
             result = joinPoint.proceed();
             long endTime = System.currentTimeMillis();
-            targetLogger.info("调用结束:{}, 返回值:{}, 耗时:{}, 描述:{}",methodName, Objects.toString(result), endTime - startTime, desc);
+            targetLogger.info("调用结束:{}, 返回值:{}, 耗时:{}, 描述:{}",
+                    methodName, objectMapper.writeValueAsString(result), endTime - startTime, desc);
         }catch (Exception ex){
-            targetLogger.info("接口调用异常:{}, 参数:{}, 描述:{}", methodName,Arrays.toString(args),desc,ex);
+            targetLogger.error("接口调用异常:{}, 参数:{}, 描述:{}",
+                    methodName, args, desc, ex);
             throw ex;
         }
         return result;
